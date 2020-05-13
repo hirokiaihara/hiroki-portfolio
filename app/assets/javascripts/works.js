53b5c9ecf5d2378b1259652828105546a5080094
hirokiaihara <hiro0818012359@gmail.com>
2020-05-13 09:54:09 +0900

    スムーススクロール・リファクタリング

--- app/assets/javascripts/works.js
+++ app/assets/javascripts/works.js
@@ -1,27 +1,19 @@
 $(document).on('turbolinks:load', function() {
-  $('.imageList').mouseover(function() {
-    var selectedSrc = $(this).attr('src').replace(/^(.+)_firstImage(\.gif|\.jpg|\.png+)$/, "$1"+"$2");
-    $('.firstImage').stop().fadeOut(50, function(){
-      $('.firstImage').attr('src', selectedSrc);
-      $('.firstImage').stop().fadeIn(200);
+  //関数定義
+  function mouseOver(imageList, firstImage) {
+    $(imageList).mouseover(function() {
+      var selectedSrc = $(this).attr('src');
+      $(firstImage).stop().fadeOut(50, function(){
+        $(this).attr('src', selectedSrc);
+        $(this).stop().fadeIn(200);
+      });
+      $(this).css({"border": "2px solid #0082c8"});
     });
-    $(this).css({"border": "2px solid #0082c8"});
-  });
-  $('.imageList').mouseout(function(){
-    $(this).css({"border": ""});
-  });
-});
-
-$(document).on('turbolinks:load', function() {
-  $('.imageList_02').mouseover(function() {
-    var selectedSrc = $(this).attr('src').replace(/^(.+)_firstImage_02(\.gif|\.jpg|\.png+)$/, "$1"+"$2");
-    $('.firstImage_02').stop().fadeOut(50, function(){
-      $('.firstImage_02').attr('src', selectedSrc);
-      $('.firstImage_02').stop().fadeIn(200);
+    $(imageList).mouseout(function(){
+      $(this).css({"border": ""});
     });
-    $(this).css({"border": "2px solid #0082c8"});
-  });
-  $('.imageList_02').mouseout(function(){
-    $(this).css({"border": ""});
-  });
+  }
+  //関数の呼び出し
+  mouseOver('.imageList_01', '.firstImage_01');
+  mouseOver('.imageList_02', '.firstImage_02');
 });
